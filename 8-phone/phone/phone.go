e0dcebee459928a396f0d9465967bcc19ef09c0f
Louis Lin <lin.louis@pm.me>
2019-08-09 09:00:12 +0200

    phone: use better normalize function

--- 8-phone/phone/phone.go
+++ 8-phone/phone/phone.go
@@ -2,10 +2,11 @@ package phone
 
 import (
 	"regexp"
-	"strings"
 )
 
-var phoneRexep = regexp.MustCompile(`[0-9]`)
+var phoneRexep = regexp.MustCompile("\\D")
+
+//var phoneRexep = regexp.MustCompile(`[0-9]`)
 
 // Phone not normalized fetched from DB
 type Phone struct {
@@ -15,11 +16,15 @@ type Phone struct {
 
 // Normalize phone to expected pattern
 func (p *Phone) Normalize() string {
-	var b strings.Builder
-	for _, c := range p.Value {
-		if phoneRexep.MatchString(string(c)) {
-			b.WriteString(string(c))
-		}
-	}
-	return b.String()
+	return phoneRexep.ReplaceAllString(p.Value, "")
 }
+
+//func (p *Phone) Normalize() string {
+//	var b strings.Builder
+//	for _, c := range p.Value {
+//		if phoneRexep.MatchString(string(c)) {
+//			b.WriteString(string(c))
+//		}
+//	}
+//	return b.String()
+//}
